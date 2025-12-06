a171241e78a1dcdcbb1d34b544e0d52415fd4280
Abdullah628 <79067872+Abdullah628@users.noreply.github.com>
2025-12-06 06:38:53 +0600

    fixed cors issues for uploading images or files

--- src/app.ts
+++ src/app.ts
@@ -21,11 +21,14 @@ app.use(expressSession({
 app.use(passport.initialize())
 app.use(passport.session())
 app.use(cookieParser())
-app.use(express.json())
 app.use(cors({
     origin: envVars.FRONTEND_URL,
-    credentials: true
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization']
 }))
+app.use(express.json({ limit: '50mb' }))
+app.use(express.urlencoded({ extended: true, limit: '50mb' }))
 
 
 app.use("/api/v1", router)
